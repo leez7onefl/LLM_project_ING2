f49c05b8fd98af4959842d21ad9bbebc6aaa7b89
leez7onefl <137315460+leez7onefl@users.noreply.github.com>
2024-12-12 14:50:42 +0100

    Create inference_test.py

---
+++ inference_test.py
@@ -0,0 +1,24 @@
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+local_model_path = "path/to/your/local/model/folder"
+
+# Load the tokenizer and model from local paths
+tokenizer = AutoTokenizer.from_pretrained(local_tokenizer_path)
+model = AutoModelForCausalLM.from_pretrained(
+  local_model_path,
+  torch_dtype="auto",
+)
+
+model.cuda()
+
+inputs = tokenizer("import torch\nimport torch.nn as nn", return_tensors="pt").to(model.device)
+
+tokens = model.generate(
+  **inputs,
+  max_new_tokens=48,
+  temperature=0.2,
+  do_sample=True,
+)
+
+print(tokenizer.decode(tokens[0], skip_special_tokens=True))
